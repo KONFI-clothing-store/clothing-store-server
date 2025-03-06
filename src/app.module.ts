a4a8b592a333a7a8f0e4d7bd0fd181692c2a7031
denismatvieiev <matvieiev.denys@student.uzhnu.edu.ua>
2025-03-06 21:57:32 +0200

    add changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,7 @@ import { PaymentModule } from './payment/payment.module';
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      url: process.env.DATABASE_URL,
       models: [User, Clothes, Comment],
       autoLoadModels: true,
     }),
